0880b9522d1dc0299fd47c5748ba2bd5b85e849b
Nalzurin <alexnormandi@ukr.net>
2025-07-20 21:25:26 +0300

    Add WorldComponent_GridBandwidth

---
+++ Source/Comps/WorldComponent_GridBandwidth.cs
@@ -0,0 +1,29 @@
+﻿using RimWorld.Planet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace CrimsonGridFramework.Comps
+{
+    public class WorldComponent_GridBandwidth : WorldComponent
+    {
+        private static WorldComponent_GridBandwidth _instance;
+        public static WorldComponent_GridBandwidth Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new WorldComponent_GridBandwidth(Find.World);
+                }
+                return _instance;
+            }
+        }
+        public WorldComponent_GridBandwidth(World world) : base(world)
+        {
+        }
+    }
+}
