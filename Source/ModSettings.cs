43e997574ba64d008b11c2be807a7875a1108c98
Ahmed Hanie <flubzd@gmail.com>
2025-10-10 14:23:52 +0300

    fix: allow mechs to carry 1 tier ligher weapons
    - Update default light weight weapons threshold setting

--- Source/ModSettings.cs
+++ Source/ModSettings.cs
@@ -9,14 +9,14 @@ namespace CrimsonGridFramework
 {
     public class ModSettings : Verse.ModSettings
     {
-        public static float lightWeaponsMassThreshold = 2.0f;
+        public static float lightWeaponsMassThreshold = 2.5f;
         public static float mediumWeaponsMassThreshold = 5.0f;
         public static float heavyWeaponsMassThreshold = 10.0f;
 
         public override void ExposeData()
         {
             base.ExposeData();
-            Scribe_Values.Look(ref lightWeaponsMassThreshold, "lightWeaponsMassThreshold", 2.0f);
+            Scribe_Values.Look(ref lightWeaponsMassThreshold, "lightWeaponsMassThreshold", 2.5f);
             Scribe_Values.Look(ref mediumWeaponsMassThreshold, "mediumWeaponsMassThreshold", 5.0f);
             Scribe_Values.Look(ref heavyWeaponsMassThreshold, "heavyWeaponsMassThreshold", 10.0f);
         }
