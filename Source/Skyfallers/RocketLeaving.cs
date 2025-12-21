083040c5e2092442db3556f0123857e67071c3d6
Nalzurin <alexnormandi@ukr.net>
2025-12-21 18:44:55 +0200

    Add dust particles on rocket launch

--- Source/Skyfallers/RocketLeaving.cs
+++ Source/Skyfallers/RocketLeaving.cs
@@ -4,15 +4,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Verse;
 
 namespace CrimsonGridFramework
 {
     public class RocketLeaving : FlyShipLeaving
     {
         //Fire and Smoke animations should go here maybe no sure yet
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            EffecterDefOf.ImpactDustCloud.Spawn(Position, map);
+
+        }
         protected override void Tick()
         {
             base.Tick();
         }
+
     }
 }
